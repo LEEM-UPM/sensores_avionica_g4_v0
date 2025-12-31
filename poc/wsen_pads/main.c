b329b57fb3cb734cf2ff215ed9a51b390d9f8f4a
MarcosMad <marcos.ocon.madrid@gmail.com>
2025-12-31 02:42:14 +0100

    Correción errores en comentarios, varialbles, funciones y magic numbers. Se añade printf para imprimir los datos y comentarios de error mediante la modificacion de la funcion __io_putchar. Y restructuración del codigo para que sea mas limpio.

--- poc/wsen_pads/main.c
+++ poc/wsen_pads/main.c
@@ -1,11 +1,29 @@
 // Example demonstrating usage of FIFO buffer in continuous mode (threshold interrupt)
+
+// ----- LIBRARIES -----
+
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
 
 #include "i2c.h"
 #include "gpio.h"
+#include "usart.h"
 
 #include "WSEN_PADS_2511020213301.h"
 #include "platform.h"
+
+// ----- CONFIGURATION MACROS -----
+
+#define DEBUG_MODE true
+
+#define FIFO_THRESHOLD_VALUE 25
+#define BUFFER_GROUP_SIZE 5
+#define BUFFER_TOTAL_GROUPS (FIFO_THRESHOLD_VALUE/BUFFER_GROUP_SIZE)
+
+
+// ----- TYPES DEFINITION -----
 
 WE_sensorInterface_t pads = {
     .sensorType = WE_PADS,
@@ -24,70 +42,120 @@ WE_sensorInterface_t pads = {
     .handle = &hi2c2
 };
 
+// ----- VARIABLE DECLARATION -----ç
+
 static volatile bool interrupt_triggered = false;
+
+int32_t pressure_buffer[PADS_FIFO_BUFFER_SIZE] = {0};
+int16_t temperature_buffer[PADS_FIFO_BUFFER_SIZE] = {0};
+uint32_t pressure_avg[BUFFER_GROUP_SIZE] = {0};
+uint16_t temperature_avg[BUFFER_GROUP_SIZE] = {0};
+
+char msg[150];
+uint32_t init_time;
+uint32_t current_time;
+int fifo_threshold = FIFO_THRESHOLD_VALUE;
+uint8_t fifoLevel;
+
+// ----- FUNCTION DECLARATION -----
+
+extern void SystemClock_Config(void);
 
 static bool mcu_init(void);
 static bool pads_init(void);
 static bool pads_start(int fifo_threshold);
+void pads_interrupt_handler();
+bool pads_compute_average_fifo_data();
+void pads_print_data();
 
-extern void SystemClock_Config(void);
 
+// ----- MAIN CODE ----- 
 int main()
 {
     mcu_init();
     pads_init();
-
-    // FIFO THRESHOLD 
-    const int fifo_threshold = 25;
-    // FIFO GROUP SIZE (number of measurements of each group of the buffer)
-    const int buffer_group_size = 5;
-    // FIFO TOTAL GRUPS (number of groups on the buffer)
-    const int buffer_total_groups = fifo_threshold/buffer_group_size;
-
     pads_start(fifo_threshold);
-    int32_t pressure_buffer[PADS_FIFO_BUFFER_SIZE] = {0};
-    PADS_state_t PADSstate;
-    uint8_t fifoLevel;
-    PADS_state_t fifoOvr;
+
+    init_time = HAL_GetTick();
+    current_time = HAL_GetTick() - init_time;
 
     while (1)
     {
-        PADS_getPressureOverrunStatus(&pads, &PADSstate);
-        PADS_getFifoFillLevel(&pads, &fifoLevel);
-        PADS_getFifoOverrunState(&pads,&fifoOvr);
+        pads_interrupt_handler();
+    }
+}
 
-        if (fifoLevel <= fifo_threshold)
+void pads_interrupt_handler()
+{
+    PADS_getFifoFillLevel(&pads, &fifoLevel);
+    if (fifoLevel <= fifo_threshold)
+    {
+        if (interrupt_triggered == true)
         {
-            if (interrupt_triggered == PADS_enable)
+            interrupt_triggered = false;
+
+            /* Get pressure measurements in one go. */
+            /* Note: Samples must be read faster than the ODR. */
+
+            if (!pads_compute_average_fifo_data())
             {
-                interrupt_triggered = false;
-
-                /* Get pressure measurements in one go. */
-                /* Note: Samples must be read faster than the ODR. */
-                
-                PADS_getFifoPressure_int(&pads, fifo_threshold, pressure_buffer);
-
-                /* Compute average of captured pressurevalues for each group of the fifo */
-                uint32_t pressure_avg[5] = {0};
-                for (uint8_t j = 0; j < buffer_total_groups; j++)
-                {
-                    uint32_t sum = 0;
-                
-                    for (uint8_t i = 0; i < buffer_group_size; i++)
-                    {
-                        sum += pressure_buffer[j*5 + i];
-                    }
-                
-                    pressure_avg[j] = sum / buffer_group_size;
-                }
+                printf("[Error]: Failed obtaining pads fifo values \r\n");
+            }
+            else
+            {
+                pads_print_data();
             }
         }
-        else
-        {
-            PADS_setFifoMode(&pads, PADS_bypassMode);
-            PADS_setFifoMode(&pads, PADS_continuousMode);
-        } 
     }
+    else
+    {
+        // Fifo threshold has been overrun (not nominal operation) 
+        // Principal cause: The fifo is not being read at time because other interrupts
+        // with highter priority are blocking it.
+        printf("[Error]: Fifo threshold has been overrun \r\n");
+        printf("[Info]: Cleaning all pads fifo values \r\n");
+        PADS_setFifoMode(&pads, PADS_bypassMode);
+        PADS_setFifoMode(&pads, PADS_continuousMode);
+    }
+}
+
+bool pads_compute_average_fifo_data()
+{
+    current_time = HAL_GetTick() - init_time;            
+
+    if (PADS_getFifoValues_int(&pads, fifo_threshold, pressure_buffer, temperature_buffer) == WE_FAIL)
+    {
+        return false;
+    }
+
+    /* Compute average of captured pressurevalues for each group of the fifo */
+
+
+    for (uint8_t j = 0; j < BUFFER_TOTAL_GROUPS; j++)
+    {
+        uint32_t sum = 0;
+        uint32_t sum2 = 0;
+    
+        for (uint8_t i = 0; i < BUFFER_GROUP_SIZE; i++)
+        {
+            sum += pressure_buffer[j*BUFFER_GROUP_SIZE + i];
+            sum2 += temperature_buffer[j*BUFFER_GROUP_SIZE + i];
+        }
+    
+        pressure_avg[j] = sum / BUFFER_GROUP_SIZE;
+        temperature_avg[j] = sum2 / BUFFER_GROUP_SIZE;
+    }
+
+    return true;
+}
+
+
+void pads_print_data()
+{
+    printf("%8" PRIu32 " %8" PRIu16 " %8" PRIu32 "\r\n",
+           pressure_avg[0],
+           temperature_avg[0],
+           current_time);
 }
 
 static bool pads_init(void)
@@ -160,7 +228,7 @@ static bool pads_start(int fifo_threshold)
     /* Activate FIFO threshold interrupt in event control register */
     PADS_setInterruptEventControl(&pads, PADS_dataReady);
 
-    /* Enable continuous operation with an update rate of 200 Hz */
+    /* Enable continuous operation with an update rate of 100 Hz */
     PADS_setOutputDataRate(&pads, PADS_outputDataRate100Hz);
 
     return true;
@@ -174,8 +242,20 @@ static bool mcu_init(void)
 
   MX_GPIO_Init();
   MX_I2C2_Init();
+  MX_USART2_UART_Init();
 
   return true;
+}
+
+// Function used by printf
+int __io_putchar(int ch)
+{
+    #if DEBUG_MODE
+        HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+        return ch;
+    #endif
+
+    return ch;
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
